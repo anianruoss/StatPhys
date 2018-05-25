eda7d43404455365136a90b5eba9c4925bf0e3d0
Anian Ruoss <anian.ruoss@gmail.com>
2018-05-25 21:26:37 +0200

    Add plotting of numatom variation

--- simulations/end-to-end-distance/plot_numatoms.py
+++ simulations/end-to-end-distance/plot_numatoms.py
@@ -6,25 +6,25 @@ from natsort import natsorted
 
 data_files = listdir('data')
 
-temperature_files = [
-    file for file in data_files if file.startswith('coords') and '_t' in file
+numatom_files = [
+    file for file in data_files if file.startswith('coords') and '_n' in file
 ]
 
-temperature_data = {'Base': {}, 'Harmonic': {}, 'Shake': {}}
+numatom_data = {'Base': {}, 'Harmonic': {}, 'Shake': {}}
 
-for temperature_file in natsorted(temperature_files):
-    simtype = temperature_file.split('coords_')[1].split('_t')[0]
+for numatom_file in natsorted(numatom_files):
+    simtype = numatom_file.split('coords_')[1].split('_n')[0]
 
-    with open(path.join('data', temperature_file)) as file:
-        temp = file.name.split('_t')[1].split('.')[0]
-        temperature_data[simtype][temp] = []
+    with open(path.join('data', numatom_file)) as file:
+        num_atoms = int(file.name.split('_n')[1].split('.')[0])
+        numatom_data[simtype][num_atoms] = []
 
         for line in file:
             if not line.startswith('MODEL'):
                 break
 
             start_atom = file.readline()
-            for i in range(6):
+            for i in range(num_atoms - 2):
                 file.readline()
             end_atom = file.readline()
             file.readline()
@@ -44,10 +44,10 @@ for temperature_file in natsorted(temperature_files):
                 dtype=float
             )
 
-            temperature_data[simtype][temp].append(
+            numatom_data[simtype][num_atoms].append(
                 np.linalg.norm(start_coords - end_coords))
 
-for simtype, data in temperature_data.items():
+for simtype, data in numatom_data.items():
     fig, ax = plt.subplots()
 
     ax.set_title(simtype)
@@ -55,8 +55,8 @@ for simtype, data in temperature_data.items():
     ax.set_ylabel('End-to-End Distance')
 
     for temp, lengths in data.items():
-        ax.plot(lengths, label=f'{temp}K')
+        ax.plot(lengths, label=f'{temp} Atoms')
 
-    ax.legend()
-    fig.savefig(f'{simtype}_temperature.pdf', bbox_inches='tight')
+    ax.legend(loc='upper left')
+    fig.savefig(f'{simtype}_numatom.pdf', bbox_inches='tight')
     plt.close(fig)
